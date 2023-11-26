8a7edf0bd603896d9b50ff6b70344c4712797cbe
Himanshu Verma <146541972+Code-Jutsuu@users.noreply.github.com>
2023-11-26 17:28:26 +0530

    Add files via upload

---
+++ 4-Array/Medium/SetMatrixZeroes.java
@@ -0,0 +1,67 @@
+
+public class SetMatrixZeroes {
+
+    public void setZeroes(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
+
+        // Check if the first row has a zero
+        for (int j = 0; j < cols; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowHasZero = true;
+                break;
+            }
+        }
+
+        // Check if the first column has a zero
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
+
+        // Mark rows and columns to be zeroed
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0; // Mark the first element in the row
+                    matrix[0][j] = 0; // Mark the first element in the column
+                }
+            }
+        }
+
+        // Set rows to zero based on the first element in the row
+        for (int i = 1; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < cols; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Set columns to zero based on the first element in the column
+        for (int j = 1; j < cols; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < rows; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Handle the first row and the first column
+        if (firstRowHasZero) {
+            for (int j = 0; j < cols; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (firstColHasZero) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
