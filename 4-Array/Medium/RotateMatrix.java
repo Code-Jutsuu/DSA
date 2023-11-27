69f669fee580877a70f360385afa1bd8695a5392
Himanshu Verma <146541972+Code-Jutsuu@users.noreply.github.com>
2023-11-27 14:39:56 +0530

    Add files via upload

---
+++ 4-Array/Medium/RotateMatrix.java
@@ -0,0 +1,50 @@
+public class RotateMatrix {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row of the transposed matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        RotateMatrix rotator = new RotateMatrix();
+
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+      //  System.out.println("Original Matrix:");
+      //  printMatrix(matrix);
+
+        rotator.rotate(matrix);
+
+        System.out.println("\nMatrix after 90-degree clockwise rotation:");
+        printMatrix(matrix);
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int num : row) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
